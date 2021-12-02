c9e297fa4a4090c634dd166322d7aaefbed5646a
Saam0 <Samvelyazhyan@gmail.com>
2021-12-02 16:10:01 +0400

    first commit

---
+++ src/test/java/com/shopmarket/ShopmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
